2a575ab3c86a18036daadbb0a7d89071da448d85
Domagoj K. Hackenberger <domagojhack@gmail.com>
2017-06-07 19:52:01 +0200

    release .15

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'KomoriVis',
+  packages = ['KomoriVis'], # this must be the same as the name above
+  version = '0.1',
+  description = 'KomoriVis: Bat call visualisation package',
+  author = 'Domagoj K. Hackenberger',
+  author_email = 'domagojhack@gmail.com',
+  url = 'https://github.com/BioQuantHr/KomoriVis', # use the URL to the github repo
+  download_url = 'https://github.com/BioQuantHr/KomoriVis/archive/0.15.tar.gz', # I'll explain this in a second
+  keywords = ['STFT', 'spectrogram', 'bat', 'calls', ''], # arbitrary keywords
+  classifiers = [],
+)
