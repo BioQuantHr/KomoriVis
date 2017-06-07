2ba5f606da60e19653371380eb37323727ee2997
Domagoj K. Hackenberger <domagojhack@gmail.com>
2017-06-07 20:20:44 +0200

    New changes for packaging

--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@ from distutils.core import setup
 setup(
   name = 'KomoriVis',
   packages = ['KomoriVis'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.15',
   description = 'KomoriVis: Bat call visualisation package',
   author = 'Domagoj K. Hackenberger',
   author_email = 'domagojhack@gmail.com',
   url = 'https://github.com/BioQuantHr/KomoriVis', # use the URL to the github repo
   download_url = 'https://github.com/BioQuantHr/KomoriVis/archive/0.15.tar.gz', # I'll explain this in a second
-  keywords = ['STFT', 'spectrogram', 'bat', 'calls', ''], # arbitrary keywords
+  keywords = ['STFT', 'spectrogram', 'bat', 'calls', 'spectrograms', 'bioacoustics', 'ultrasound'], # arbitrary keywords
   classifiers = [],
+  install_requires=[
+          'numpy',
+          'scipy',
+          'matplotlib',
+          'pylab',
+      ],
 )
